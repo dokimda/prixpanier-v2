f17304cb299fe421388586b875a4478731fedab5
dokimda <do.kimda@gmail.com>
2026-05-15 17:52:07 +0200

    Delete logic.js

--- logic.js
+++
@@ -1,8 +0,0 @@
-export function pricePerKg(price, quantity, unit) {
-  let base = quantity;
-
-  if (unit === 'g') base = quantity / 1000;
-  if (unit === 'ml') base = quantity / 1000;
-
-  return price / base;
-}
