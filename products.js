35499a2ed55ae7be768de1b4adff856ad8addf2b
dokimda <do.kimda@gmail.com>
2026-05-15 17:52:43 +0200

    Delete products.js

--- products.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json([
-    { id: 1, name: 'Pâtes', store: 'Carrefour', price: 1.89, quantity: 500, unit: 'g' },
-    { id: 2, name: 'Pâtes', store: 'Colruyt', price: 3.2, quantity: 1, unit: 'kg' },
-    { id: 3, name: 'Riz', store: 'Lidl', price: 2.6, quantity: 1, unit: 'kg' }
-  ]);
-}
